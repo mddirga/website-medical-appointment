e88e2f2d35fafa74926bc738eee81e63bdf2d198
mddirga <madedirga22@gmail.com>
2025-10-31 15:48:28 +0800

    Fillable Relationship Model

--- app/Models/Hospital.php
+++ app/Models/Hospital.php
@@ -3,8 +3,40 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Facades\Storage;
 
 class Hospital extends Model
 {
     //
+    use SoftDeletes;
+
+    protected $fillable = [
+        'name',
+        'photo',
+        'about',
+        'address',
+        'city',
+        'phone',
+        'postal_code',
+    ];
+
+    public function doctors()
+    {
+        return $this->hasMany(Doctor::class);
+    }
+
+    public function specialists()
+    {
+        return $this->belongsToMany(Specialist::class, 'hospital_specialists');
+    }
+
+    public function getPhotoAttribute($value)
+    {
+        if(!$value) {
+            return null;
+        }
+
+        return url(Storage::url($value));
+    }
 }
