1947e4513c7b937480c53c64c6c1bb651318b59f
mddirga <madedirga22@gmail.com>
2025-11-15 11:06:55 +0800

    Add Doctor Repository Service

--- app/Repositories/HospitalSpecialistRepository.php
+++ app/Repositories/HospitalSpecialistRepository.php
@@ -2,7 +2,14 @@
 
 namespace App\Repositories;
 
+use App\Models\HospitalSpecialist;
+
 class HospitalSpecialistRepository
 {
-
+    public function existsForHospitalAndSpecialist(int $hospitalId, int $specialistId)
+    {
+        return HospitalSpecialist::where('hospital_id', $hospitalId)
+            ->where('specialist_id', $specialistId)
+            ->exists();
+    }
 }
--- app/Services/DoctorService.php
+++ app/Services/DoctorService.php
@@ -2,7 +2,100 @@
 
 namespace App\Services;
 
+use App\Repositories\DoctorRepository;
+use App\Repositories\HospitalSpecialistRepository;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Validation\ValidationException;
+
 class DoctorService
 {
+    private $doctorRepository;
+    private $hospitalSpecialistRepository;
+
+    public function __construct
+    (
+        DoctorRepository $doctorRepository,
+        HospitalSpecialistRepository $hospitalSpecialistRepository
+    )
+    {
+        $this->doctorRepository = $doctorRepository;
+        $this->hospitalSpecialistRepository = $hospitalSpecialistRepository;
+    }
+
+    public function getAll(array $fields)
+    {
+        return $this->doctorRepository->getAll($fields);
+    }
+
+    public function getById(int $id, array $fields)
+    {
+        return $this->doctorRepository->getById($id, $fields);
+    }
+
+    public function create(array $data)
+    {
+        if(!$this->hospitalSpecialistRepository->existsForHospitalAndSpecialist(
+            $data['hospital_id'],
+            $data['specialist_id']
+        )) {
+            throw ValidationException::withMessages([
+                'specialist_id' => ['The selected specialist is not available in the chosen hospital.']
+            ]);
+        }
+
+        if(isset($data['photo']) && $data['photo'] instanceof UploadedFile) {
+            $data['photo'] = $this->uploadPhoto($data['photo']);
+        }
+
+        return $this->doctorRepository->create($data);
+    }
+
+    public function update(int $id, array $data)
+    {
+        if(!$this->hospitalSpecialistRepository->existsForHospitalAndSpecialist(
+            $data['hospital_id'],
+            $data['specialist_id']
+        )) {
+            throw ValidationException::withMessages([
+                'specialist_id' => ['The selected specialist is not available in the chosen hospital.']
+            ]);
+        }
+
+        $doctor = $this->doctorRepository->getById($id, ['*']);
+
+        if(isset($data['photo']) && $data['photo'] instanceof UploadedFile) {
+            if(!empty($doctor->photo)){
+                $this->deletePhoto($doctor->photo);
+            }
+            $data['photo'] = $this->uploadPhoto($data['photo']);
+        }
+    }
+
+    public function delete(int $id)
+    {
+        $doctor = $this->doctorRepository->getById($id, ['*']);
+
+        if ($doctor->photo) {
+            $this->deletePhoto($doctor->photo);
+        }
+
+        $this->doctorRepository->delete($id);
+    }
+
+    private function uploadPhoto(UploadedFile $photo)
+    {
+        return $photo->store('doctors', 'public');
+    }
+
+    private function deletePhoto(string $photoPath)
+    {
+        $relativePath = 'doctors/' . basename($photoPath);
+        if (Storage::disk('public')->exists($relativePath)){
+            Storage::disk('public')->delete($relativePath);
+        }
+    }
+
+
 
 }
